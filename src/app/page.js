85263acec2194afdf50f5e6b3cb02e6b81a2eb32
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-10 16:29:29 -0300

    Criando função fetch

--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,35 @@
 import { CarPost } from "@/components/CardPost";
 
-const post = {
-  "id": 1,
-  "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
-  "title": "Introdução ao React",
-  "slug": "introducao-ao-react",
-  "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
-  "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
-  "author": {
-    "id": 101,
-    "name": "Ana Beatriz",
-    "username": "anabeatriz_dev",
-    "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
-  }
-};
+// const post = {
+//   "id": 1,
+//   "cover": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/posts/introducao-ao-react.png",
+//   "title": "Introdução ao React",
+//   "slug": "introducao-ao-react",
+//   "body": "Neste post, vamos explorar os conceitos básicos do React, uma biblioteca JavaScript para construir interfaces de usuário. Vamos cobrir componentes, JSX e estados.",
+//   "markdown": "```javascript\nfunction HelloComponent() {\n  return <h1>Hello, world!</h1>;\n}\n```",
+//   "author": {
+//     "id": 101,
+//     "name": "Ana Beatriz",
+//     "username": "anabeatriz_dev",
+//     "avatar": "https://raw.githubusercontent.com/viniciosneves/code-connect-assets/main/authors/anabeatriz_dev.png"
+//   }
+// };
 
-export default function Home() {
+async function getAllPots() {
+  const response = await fetch('http://localhost:3042/posts');
+
+  if(!response.ok) {
+    console.log("Ops, alguma coisa ocorreu mal");
+  }
+  return response.json();
+}
+
+export default async function Home() {
+  const posts = await getAllPots();
+
   return (
     <main>
-      <CarPost post={post}/>
+      {posts.map(post => <CarPost post={post}/>)};      
     </main>
   );
 }
