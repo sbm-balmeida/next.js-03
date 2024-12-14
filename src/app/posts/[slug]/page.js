71fd773ae325524096883744d1ecf3a20db3079c
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-14 16:51:27 -0300

    Obtendo o post de forma assincrona

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,4 +1,24 @@
+async function getPostBySlug(slug) {
+    const url = `http://localhost:3042/posts?slug=${slug}`;
+    const response = await fetch(url);
+    
+    if (!response.ok) {
+        logger.error('Ops, alguma coisa correu mal');
+        return {};
+    }
+    logger.info('Posts obtidos com sucesso');
+    
+    const data = await response.json();
+    
+    if (data.length == 0) {
+        return {};
+    }
+    return data[0];
+}
+
 const PagePost = () => {
+    const post = getPostBySlug(???)
+
     return <h1>Olá posts</h1>
 }
 
--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,30 +1,33 @@
 import Image from "next/image";
 import { Avartar } from "../Avatar";
 import styles from './cardpost.module.css';
+import Link from "next/link";
 
 export const CarPost = ({ post }) => {
     return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure>
-                    <Image 
-                        src={post.cover} 
-                        width={438} 
-                        height={133} 
-                        alt={`Capa do post de título: ${post.title}`}
+        <Link href={`/posts/${post.slug}`} className={styles.link}>
+            <article className={styles.card}>
+                <header className={styles.header}>
+                    <figure>
+                        <Image
+                            src={post.cover}
+                            width={438}
+                            height={133}
+                            alt={`Capa do post de título: ${post.title}`}
+                        />
+                    </figure>
+                </header>
+                <section className={styles.body}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                </section>
+                <footer className={styles.footer}>
+                    <Avartar
+                        imageSrc={post.author.avatar}
+                        name={post.author.username}
                     />
-                </figure>
-            </header>
-            <section className={styles.body}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-            </section>
-            <footer className={styles.footer}>
-                <Avartar 
-                    imageSrc={post.author.avatar} 
-                    name={post.author.username}
-                />
-            </footer>
-        </article>
+                </footer>
+            </article>
+        </Link>
     )
 }
