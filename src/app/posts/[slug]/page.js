a7eb0fd8afdcb587988b220466c3d1b30eff6ce5
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-12-14 17:12:04 -0300

    Mão na massa: obtendo o post

--- src/app/posts/[slug]/page.js
+++ src/app/posts/[slug]/page.js
@@ -1,27 +1,24 @@
-import logger from "@/logger";
+// import logger from "@/logger"
 
-async function getPostBySlug(slug) {
-    const url = `http://localhost:3042/posts?slug=${slug}`;
-    const response = await fetch(url);
-    
+async function getPostBySlug (slug) {
+    const url  = `http://localhost:3042/posts?slug=${slug}`
+    const response = await fetch(url)
     if (!response.ok) {
-        logger.error('Ops, alguma coisa correu mal');
-        return {};
+    //   logger.error('Ops, alguma coisa correu mal')
+      return {}
     }
-    logger.info('Posts obtidos com sucesso');
-    
-    const data = await response.json();
-    
+    // logger.info('Posts obtidos com sucesso')
+    const data = await response.json()
     if (data.length == 0) {
-        return {};
+        return {}
     }
+
     return data[0];
-}
+} 
 
-const PagePost = ({ params }) => {
-    const post = getPostBySlug(params.slug)
-
+const PagePost = async ({ params }) => {
+    const post = await getPostBySlug(params.slug)
     return <h1 style={{ color: 'white' }}>{post.title}</h1>
 }
 
-export default PagePost;
+export default PagePost
